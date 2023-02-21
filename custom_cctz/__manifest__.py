1921311757eb0e46bf474e544345e49de855af97
lumanya <shaibulumanya@gmail.com>
2023-02-21 12:13:12 +0300

    [ADD] version 16.0.1.0

--- custom_cctz/__manifest__.py
+++ custom_cctz/__manifest__.py
@@ -14,12 +14,15 @@
         'contacts',
         'mail',
         'purchase'
+       
+        
     ],
     'data': [
     'security/ir.model.access.csv',
     'views/crm_account_type.xml',
     'views/crm_payment_terms.xml',
     'views/crm_purchase_time_frame.xml',
+    'views/crm_status.xml',
     'views/crm_inherit_crm_lead_view.xml',
     'views/product_manufacuture.xml',
     'views/product_inherit_product_template.xml',
@@ -27,9 +30,11 @@
     'views/market_category.xml',
     'views/customer_type.xml',
     'views/inherit_res_partner.xml'
+    
     ],
     'images': ['static/description/banner.png'],
     'installable': True,
     'application': True,
     'autoinstall': False,
-}
+   
+    }
